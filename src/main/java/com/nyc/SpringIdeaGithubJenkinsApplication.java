069cc9939ad956fa3cb7d22a094f9fb40d67d1ea
nyc0668 <814363070@qq.com>
2021-04-24 19:36:18 +0800

    first commit

---
+++ src/main/java/com/nyc/SpringIdeaGithubJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.nyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIdeaGithubJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIdeaGithubJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyc/SpringIdeaGithubJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdeaGithubJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
