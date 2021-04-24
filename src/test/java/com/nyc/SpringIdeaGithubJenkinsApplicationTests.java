fc13d18eb3603cb8864dc5c604cc31ad2c288250
nyc0668 <814363070@qq.com>
2021-04-24 21:02:43 +0800

    hahaha

--- src/test/java/com/nyc/SpringIdeaGithubJenkinsApplicationTests.java
+++ src/test/java/com/nyc/SpringIdeaGithubJenkinsApplicationTests.java
@@ -9,6 +9,8 @@ class SpringIdeaGithubJenkinsApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
     }
 
 }
