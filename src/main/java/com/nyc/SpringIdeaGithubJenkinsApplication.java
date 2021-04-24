47a416f055c0949adf2198a7f65f5b3987c7da53
nyc0668 <814363070@qq.com>
2021-04-24 20:35:07 +0800

    second commit

--- src/main/java/com/nyc/SpringIdeaGithubJenkinsApplication.java
+++ src/main/java/com/nyc/SpringIdeaGithubJenkinsApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringIdeaGithubJenkinsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringIdeaGithubJenkinsApplication.class, args);
+
+        SpringApplication.run(SpringIdeaGithubJenkinsApplication.class, args
+
+        );
     }
 
 }
--- src/test/java/com/nyc/SpringIdeaGithubJenkinsApplicationTests.java
+++ src/test/java/com/nyc/SpringIdeaGithubJenkinsApplicationTests.java
@@ -8,6 +8,7 @@ class SpringIdeaGithubJenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("a");
     }
 
 }
